eacf93549c89b19ad73a5bde94b66026467ca969
FraErriques <francesco.erriques@gmail.com>
2022-01-03 17:13:52 +0100

    suspended jobs on ITBZ.

--- TestConsole/src/testconsole/TestConsole.java
+++ TestConsole/src/testconsole/TestConsole.java
@@ -33,7 +33,12 @@ public class TestConsole
 		// between the token instanceName and the actual instance-name.
         //Update the username and password below
         //String connectionUrl   = "jdbc:sqlserver://Kronecker;instanceName=Delta;databaseName=PrimeData;user=sa;password=Riemann0";
-        String connectionUrl   = "jdbc:sqlserver://Cantor;databaseName=PrimeData;user=sa;password=Riemann0";
+        //String connectionUrl   = "jdbc:sqlserver://Cantor;databaseName=PrimeData;user=sa;password=Riemann0";
+                //String connectionUrl = "jdbc:sqlserver://192.168.30.63;instanceName=SqlExpress:1434;databaseName=dotazioni2021;user=sa;password=M1 Sxpdx";
+        //String connectionUrl = "jdbc:sqlserver://192.168.30.63;instanceName=ExpressLie:1434;databaseName=dotazioni2021;user=sa;password=M1 Sxpdx";
+        String connectionUrl   = "jdbc:sqlserver://ITBZOW1422;instanceName=Delta;databaseName=dotazioni2021;user=sa;password=M1 Sxpdx";
+        //String connectionUrl   = "jdbc:sqlserver://ITBZOW1422;instanceName=SqlExpress;databaseName=Logging;user=sa;password=M1 Sxpdx";
+        //String connectionUrl   = "jdbc:sqlserver://ITBZOW1422;instanceName=ExpressLie;databaseName=Logging;user=sa;password=M1 Sxpdx";
         //
         try
         {
@@ -90,8 +95,8 @@ public class TestConsole
     /******************* EntryPoint ****** Starting BrikoJ::WinPlatf from 11/november/2021 *****************/	
     public static void main(String[] args)
     {
-        //postgreSQLconn();
-        msSqlconn();
+        postgreSQLconn();
+        //msSqlconn();
         
         
         // String connectionUrl = "jdbc:sqlserver://ITBZOW1422;instanceName=SqlExpress:1433;databaseName=dotazioni2021;user=sa;password=M1 Sxpdx";
